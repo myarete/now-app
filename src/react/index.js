2cf9b3348985fe324035bbf98cf77ade8fda4626
Leo Lamprecht <mindrun@icloud.com>
2016-08-17 16:41:18 +0200

    Multiple envs for linting

--- src/react/index.js
+++ src/react/index.js
@@ -1,4 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<div>Hello world!</div>)
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
+)
