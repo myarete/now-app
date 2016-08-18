c3d9352c174d16934b9841299b151a77b1968f8c
Leo Lamprecht <mindrun@icloud.com>
2016-08-18 18:09:37 +0200

    Treat electron as external dependency

--- rollup.config.electron.js
+++ rollup.config.electron.js
@@ -1,8 +1,8 @@
 import babel from 'rollup-plugin-babel'
 import json from 'rollup-plugin-json'
+import nodeResolve from 'rollup-plugin-node-resolve'
 
 const external = [
-  'path',
   'electron'
 ]
 
@@ -21,6 +21,11 @@ export default {
   dest: './app/dist/electron.js',
   plugins: [
     json(),
+    nodeResolve({
+      jsnext: true,
+      main: true,
+      module: true
+    }),
     babel({
       plugins: [
         'transform-async-to-generator'
