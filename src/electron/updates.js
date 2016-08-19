49092ac940a66293f2f92887d3eca28011112365
Leo Lamprecht <mindrun@icloud.com>
2016-08-19 14:40:44 +0200

    Make auto updates work

--- src/electron/updates.js
+++ src/electron/updates.js
@@ -4,15 +4,15 @@ import ms from 'ms'
 import notify from 'display-notification'
 
 // Ours
+import {version} from '../../package'
 import {error as showError} from './dialogs'
 
-// const platform = process.platform ? 'osx' : process.platform === 'darwin'
-// const feedURL = 'https://hyperterm-updates.now.sh/update/' + platform
+const platform = process.platform ? 'osx' : process.platform === 'darwin'
+const feedURL = 'https://now-updates.now.sh/update/' + platform
 
 export default () => {
   autoUpdater.on('error', showError)
-
-  // autoUpdater.setFeedURL(feedURL + '/' + app.getVersion())
+  autoUpdater.setFeedURL(feedURL + '/' + version)
 
   setTimeout(autoUpdater.checkForUpdates, ms('10s'))
   setInterval(autoUpdater.checkForUpdates, ms('5m'))
