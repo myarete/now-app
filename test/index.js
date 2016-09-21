0cfc8a1a4c5813210b00b26bccb4ff5d4ee6dbfe
Leo Lamprecht <mindrun@icloud.com>
2016-09-21 12:17:25 +0200

    Use a single app instance of all tests

--- test/index.js
+++ test/index.js
@@ -5,28 +5,26 @@ import path from 'path'
 import test from 'ava'
 import {Application} from 'spectron'
 
-test.beforeEach(async t => {
-  t.context.app = new Application({
+let app
+
+test.before(async () => {
+  app = new Application({
     path: path.join(__dirname, '../dist/mac/Now.app/Contents/MacOS/Now')
   })
 
-  await t.context.app.start()
+  await app.start()
 })
 
-test.afterEach.always(async t => {
-  await t.context.app.stop()
+test.after(async () => {
+  await app.stop()
 })
 
 test('check window count', async t => {
-  const app = t.context.app
   await app.client.waitUntilWindowLoaded()
-
   t.is(await app.client.getWindowCount(), 4)
 })
 
 test('see if dev tools are open', async t => {
-  const app = t.context.app
   await app.client.waitUntilWindowLoaded()
-
   t.false(await app.browserWindow.isDevToolsOpened())
 })
